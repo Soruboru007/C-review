a1e66c8639465449de1db9c4fcc1636bc145ffde
soru <ityouna007@gmail.com>
2025-02-11 07:44:57 +0900

    feat:argcのエラー処理を書いた

--- basic/test-function/argcv3.c
+++ basic/test-function/argcv3.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 int main (int argc, char *argv[])
 {
+  if(argc < 3 || argc > 3)
+  {
+    printf("エラー: 2つの整数を入力してください。\n");
+    return 1;
+  }
   int num;
   num = atoi(argv[1]) * atoi(argv[2]);
   printf("積は%d\n",num);
