b5dbc9ef4c34d1ac3841edf4af38876843a4bed4
soru <ityouna007@gmail.com>
2025-02-21 23:03:56 +0900

    act 1

--- basic/review/countdown.c
+++ basic/review/countdown.c
@@ -1,7 +1,10 @@
 #include <unistd.h>
 
-int		main(void)
+int		main(int ac, char **av)
 {
-	write(1, "aBcDeFgHiJkLmNoPqRsTuVwXyZ\n", 27);
+	if (ac > 1)
+		while (*av[1])
+			write(1, av[1]++, 1);
+	write(1, "\n", 1);
 	return (0);
 }
